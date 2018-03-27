45d33ac6c5294d3fb50e40b48f006c8efed37898
Svante P <svantepagels@gmail.com>
2018-03-27 13:15:23 +0200

    Second try

--- Labb/ComputerLab2.R
+++ Labb/ComputerLab2.R
@@ -1,1 +1,19 @@
-## 
+library(plyr)
+# Load data
+sleep <- read.delim("sleep.txt")
+
+# Turn danger into categorical data
+sleep$Danger <- factor(sleep$Danger, levels=c(1,2,3),
+                       labels=c("low","medium","high"))
+
+# Count frequency of danger
+count(sleep$Danger)
+
+# b) Calculate mean sleep for different danger categories
+t <- aggregate(TotalSleep~Danger , data=sleep , FUN=mean)
+
+# Boxplot
+plot(TotalSleep~Danger, data=sleep)
+
+# c) Multiple linear regression
+model <- lm(TotalSleep~Danger.medium+Danger.high, data=sleep)
